97a7615cc289f375f7b3c73ccab940cc1f07c796
Karolina.Gil <karol@Karolinas-pc>
2023-09-26 21:24:13 +0200

    api status messages print out

--- app/app.py
+++ app/app.py
@@ -26,8 +26,13 @@ def upload():
     file = request.files['jsonFile']
     dmp = json.load(file)['dmp']
 
+    projectComplete = "Project creation failed."
+    accountComplete = []
+
     # 1. Create a new project
-    res = seek_client.create_project()
+    res = seek_client.create_project(dmp['title'])
+    if res.status_code == 200:
+        projectComplete = "Project created succesfully"
     print(res.status_code)
 
     # 2. Create users for all contributors
@@ -35,9 +40,17 @@ def upload():
         name = person['name']
         email = person['mbox']
         res = seek_client.create_person(name, email)
-        print(res.status_code)
+        if res.status_code == 200: #correct went thru
+            message = "User " + str(name) + " was succesfully created. " + str(email)
+            accountComplete.append(message)
+        elif res.status_code == 422: #Unprocessable Entity / duplicate
+            message = "User " + str(name) + " has failed to be created. " + str(email)
+            accountComplete.append(message) #this might be changed out with "status": "Account already exists"
 
-    return render_template('./upload.html')
+        print(res.status_code)
+        
+
+    return render_template('./upload.html', projectStatus = projectComplete, accountStatus = accountComplete)
 
 
 if __name__ == '__main__':
