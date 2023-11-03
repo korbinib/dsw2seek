752566a882745695a310c9892ea4fafd2b4b5387
Markus <markus.sylta@student.uib.no>
2023-11-03 11:09:29 +0100

    Give correct response to DSW from SEEK

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import base64
 from flask import Flask, render_template, request, jsonify
 from waitress import serve
 from api.seek import SeekClient
+from http.client import responses
 
 app = Flask(__name__)
 
@@ -47,6 +48,7 @@ def upload():
     Upload page. Processes the uploaded DMP file and creates a new project and users in SEEK.
     '''
     dmp = load_file(request)
+    check_DSW = isDSW(request)
     institution = request.form.get('institutionId')
 
     # 1. Create users for all contributors
@@ -64,17 +66,27 @@ def upload():
 
     project['response'] = {
         'status_code': res.status_code, 'json': res.json()}
-
+    if check_DSW:
+        if res.status_code == 422:
+            return ('Project already exists in SEEK.', res.status_code)
+        elif res.status_code == 200:
+            return render_template('./upload.html', people=people, project=project)
+        else:
+            return ('SEEK returned following status: ', responses[res.status_code], res.status_code)
     return render_template('./upload.html', people=people, project=project)
+    
+
+    
 
 
 def load_file(request):
     '''
     Account for different ways of sending the file.
     Requests from this website will put the file in request.files,
-    but DSW will put it in request.form.
+    but DSW will put it in request.form. Uses isDSW to check where its
+    request originates from.
     '''
-    if len(request.files) == 0:
+    if isDSW(request):
         # This is a request from DSW. Create a Seek client with the given credentials.
         global seek_client
         seek_client = SeekClient(
@@ -82,9 +94,16 @@ def load_file(request):
         file = request.form['jsonFile']
         return json.loads(file)['dmp']
     else:
+        # This is a request from HTML.
         file = request.files['jsonFile']
         return json.load(file)['dmp']
 
+def isDSW(request):
+    '''
+    Does a quick check for where the file originates from. If its from DSW the
+    request uses form, and therefore will return 0 from 'request.files'.
+    '''
+    return len(request.files) == 0
 
 if __name__ == '__main__':
     print('Server running at http://localhost:8080')
